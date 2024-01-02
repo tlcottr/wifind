85c8652d6054a8f56b701c86b6b079ea331686f0
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2024-01-01 20:39:03 -0500

    Update splash.tsx

--- pages/splash.tsx
+++ pages/splash.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import { Tooltip } from "react-tooltip";
